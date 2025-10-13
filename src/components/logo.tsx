4ff2f65c398163f129e4730044bda27018e3badb
Umashankar s <UShankar706@gmail.com>
2025-10-13 09:55:46 +0000

    Lobby got cut and not visible (_for element text_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,12 +4,12 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 200 50"
-      width="160"
+      viewBox="0 0 250 50"
+      width="200"
       height="40"
       {...props}
     >
-      <rect width="200" height="50" fill="transparent" />
+      <rect width="250" height="50" fill="transparent" />
       <path
         d="M25 10 C15 10, 10 15, 10 25 C10 45, 25 50, 25 50 C25 50, 40 45, 40 25 C40 15, 35 10, 25 10 Z"
         fill="hsl(var(--primary))"

3b33614d38ad12acc3c03e4a2b4ef1dc78655973
Umashankar s <UShankar706@gmail.com>
2025-10-16 12:45:47 +0000

    make the App name ti VisiTrack

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
-  title: 'Smart Lobby',
+  title: 'VisiTrack',
   description: 'A modern visitor management system.',
 };
 
